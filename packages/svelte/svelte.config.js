d15052829e6eb0f8050f684b7e2e872eb0067b2b
Don McCurdy <dm@donmccurdy.com>
2023-05-07 19:40:24 -0400

    Packaging.

--- packages/svelte/svelte.config.js
+++ packages/svelte/svelte.config.js
@@ -3,12 +3,6 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	package: {
-		dir: 'dist'
-	},
-
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
