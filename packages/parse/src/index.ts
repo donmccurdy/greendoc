0b8579068b8afbb5c16bd62f8eb39edb10ab51af
Don McCurdy <dm@donmccurdy.com>
2023-05-09 14:17:25 -0400

    Add test fn and enum for the build

--- packages/parse/src/index.ts
+++ packages/parse/src/index.ts
@@ -1,3 +1,15 @@
 export * from './Parser';
 export * from './Encoder';
 export * from './types';
+
+/** This is test function, because the compiler wants something in the /functions path. */
+export function testFunction(a: number, b: number): string {
+	return 'hello world';
+}
+
+/** This is test enum, because the compiler wants something in the /enums path. */
+export enum TestEnum {
+	A,
+	B,
+	C
+}
