97c2aedae660e28273a112b0f6b8c7785b2b71e5
Don McCurdy <dm@donmccurdy.com>
2023-06-22 16:40:07 -0400

    Remove workaround for missing kinds

--- packages/parse/src/index.ts
+++ packages/parse/src/index.ts
@@ -1,15 +1,3 @@
 export * from './Parser';
 export * from './Encoder';
 export * from './types';
-
-/** TODO(bug): This is test function, because the compiler wants something in the /functions path. */
-export function testFunction(a: number, b: number): string {
-	return 'hello world';
-}
-
-/** TODO(bug): This is test enum, because the compiler wants something in the /enums path. */
-export enum TestEnum {
-	A,
-	B,
-	C
-}
