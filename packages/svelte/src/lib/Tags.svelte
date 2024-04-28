5c689c85a33730db7f2d3e58b816d0b6893096a1
Don McCurdy <dm@donmccurdy.com>
2024-04-28 18:28:05 -0400

    Limit displayed tags

--- packages/svelte/src/lib/Tags.svelte
+++ packages/svelte/src/lib/Tags.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+	const DISPLAYED_TAGS = new Set<string>(['alpha', 'beta', 'experimental', 'deprecated']);
+
 	export let data: Record<string, string | true>;
+
+	function hasVisibleTag(): boolean {
+		for (const tag in data) {
+			if (DISPLAYED_TAGS.has(tag)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	function getTagTitle(tagComment: string | true): string | null {
 		if (typeof tagComment === 'string') {
@@ -9,10 +20,14 @@
 	}
 </script>
 
-<ul class="tags">
-	{#each Object.keys(data) as tag (tag)}
-		<li class="tag" data-gd-tag={tag} title={getTagTitle(data[tag])}>
-			{tag}
-		</li>
-	{/each}
-</ul>
+{#if hasVisibleTag()}
+	<ul class="tags">
+		{#each Object.keys(data) as tag (tag)}
+			{#if DISPLAYED_TAGS.has(tag)}
+				<li class="tag" data-gd-tag={tag} title={getTagTitle(data[tag])}>
+					{tag}
+				</li>
+			{/if}
+		{/each}
+	</ul>
+{/if}
